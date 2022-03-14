315cef6236998ba61c15cae5e3546d276619933f
HIMATEKTRO <100829918+HIMATEKTRO@users.noreply.github.com>
2022-03-14 10:29:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ while ($row=mysqli_fetch_array($query)) {
               <a href="news-details.php?nid=<?php echo htmlentities($row['pid'])?>" class="btn btn-primary">Read More &rarr;</a>
             </div>
             
-            <img class="card-img-top" src="admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
+            <img class="card-img-top" src="admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>" style="width:500px; height:500px; margin-left:30px; margin-bottom:30px;">
             
             <div class="card-footer text-muted">
               Posted on <?php echo htmlentities($row['postingdate']);?>
