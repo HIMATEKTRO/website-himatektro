6745f36822d7b0749bed4792d2668eab82662448
HIMATEKTRO <100829918+HIMATEKTRO@users.noreply.github.com>
2022-03-14 11:32:15 +0700

    Update search.php

--- search.php
+++ search.php
@@ -83,7 +83,9 @@ while ($row=mysqli_fetch_array($query)) {
       
             <div class="card-body">
               <h2 class="card-title"><?php echo htmlentities($row['posttitle']);?></h2>
-         
+              
+              <p><?php $pt=$row['postdetails']; echo  (substr($pt, 0, 110));?> ...</p>
+              
               <a href="news-details.php?nid=<?php echo htmlentities($row['pid'])?>" class="btn btn-primary">Read More &rarr;</a>
             </div>
             <div class="card-footer text-muted">
