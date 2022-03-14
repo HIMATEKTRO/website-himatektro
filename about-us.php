fc2e69a6eafa93e5c1d79f964c1881557cb66029
HIMATEKTRO <100829918+HIMATEKTRO@users.noreply.github.com>
2022-03-14 09:26:05 +0700

    Update about-us.php

--- about-us.php
+++ about-us.php
@@ -12,7 +12,7 @@ include('includes/config.php');
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>HIMATEKTRO | About us</title>
+    <title> About us | HIMPUNAN MAHASISWA TEKNIK ELEKTRO | UNIVERSITAS SYIAH KUALA </title>
 
     <!-- JavaScript FontAwesome -->
     <script src="https://kit.fontawesome.com/ad0d081d8e.js" crossorigin="anonymous"></script>
