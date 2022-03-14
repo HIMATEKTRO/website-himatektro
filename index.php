ca8a9a8837b7ae7b92f7fd7fe05a4e4e6b521108
HIMATEKTRO <100829918+HIMATEKTRO@users.noreply.github.com>
2022-03-14 10:22:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -64,17 +64,20 @@ while ($row=mysqli_fetch_array($query)) {
 ?>
 
           <div class="card mb-4">
- <img class="card-img-top" src="admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
             <div class="card-body">
               <h2 class="card-title"><?php echo htmlentities($row['posttitle']);?></h2>
                  <p><b>Category : </b> <a href="category.php?catid=<?php echo htmlentities($row['cid'])?>"><?php echo htmlentities($row['category']);?></a> </p>
        
               <a href="news-details.php?nid=<?php echo htmlentities($row['pid'])?>" class="btn btn-primary">Read More &rarr;</a>
             </div>
+            
+            <img class="card-img-top" src="admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
+            
             <div class="card-footer text-muted">
               Posted on <?php echo htmlentities($row['postingdate']);?>
            
             </div>
+            
           </div>
 <?php } ?>
        
