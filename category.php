b9bd83ca5e57091b1587cbad46a17438e6cf0e9b
HIMATEKTRO <100829918+HIMATEKTRO@users.noreply.github.com>
2022-03-14 11:30:15 +0700

    Update category.php

--- category.php
+++ category.php
@@ -86,7 +86,7 @@ while ($row=mysqli_fetch_array($query)) {
           <div class="card mb-4">
             <div class="card-body">
               <h2 class="card-title"><?php echo htmlentities($row['posttitle']);?></h2>
-              <p><?php echo $row['postdetails'];?></p>
+              <p><?php $pt=$row['postdetails']; echo  (substr($pt, 0, 110));?> ...</p>
               <a href="news-details.php?nid=<?php echo htmlentities($row['pid'])?>" class="btn btn-primary">Read More &rarr;</a>
             </div>
             <img class="card-img-top" src="admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>" style="max-width:auto; height:auto; margin:auto; margin-bottom:auto;">
