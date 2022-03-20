f0126acb77cd47bc07a6be3263f96ceae93e3549
HIMATEKTRO <100829918+HIMATEKTRO@users.noreply.github.com>
2022-03-20 14:07:23 +0700

    Create 404page.php

---
+++ 404page.php
@@ -0,0 +1,68 @@
+<?php
+include('includes/config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title> 404 error halaman tidak ditemukan | HIMPUNAN MAHASISWA TEKNIK ELEKTRO | UNIVERSITAS SYIAH KUALA </title>
+    
+    <!-- Set favicon icon in head-->
+    <link rel="shortcut icon" type="image/png" href="./himatektro-icon.png">
+
+    <!-- JavaScript FontAwesome -->
+    <script src="https://kit.fontawesome.com/ad0d081d8e.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body style="margin: auto;">
+    <!-- Navigation -->
+    <?php include('includes/header.php');?>
+    <!-- Page Content -->
+    <div class="container">
+      <div class="row" style="margin-top: 4%">
+    
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+        <br /><br /><br />
+
+          <!-- Blog Post -->
+          <div>
+          <br />
+            <h5 style="text-align: center; margin-top: auto;">No pages found</h5>
+            
+          </div>
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+        <?php include('includes/sidebar.php');?>
+      </div>
+    
+    </div>
+    
+    <!-- /.container -->
+
+    <!-- Footer -->
+ <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  
+
+  </body>
+
+</html>
