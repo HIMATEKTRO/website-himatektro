82881a72ea4862dbcf04068c7196ca8d8fe06166
HIMATEKTRO <100829918+HIMATEKTRO@users.noreply.github.com>
2022-03-20 14:02:13 +0700

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -1,31 +1,40 @@
-<div class="header">
-  <nav class="navbar navbar-expand-lg navbar-light bg-dark bg-opacity-75 fixed-top">
+<div class="header pb-5">
+  <nav class="navbar navbar-expand-md navbar-light bg-dark bg-opacity-80 fixed-top">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">
+      <a class="navbar-brand" aria-current="page" href="index.php">
         <img src="images/LogoHimatektro.png" alt="" width="156" height="54" class="d-inline-block"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link active text-white" aria-current="page" href="berita.php">BERITA</a>
-          <a class="nav-link text-white" href="departemen.php">DEPARTEMEN</a>
-          <a class="nav-link text-white" href="#">DOKUMEN</a>
-          <a class="nav-link text-white" href="#">PROFIL</a>
-          
-        </div>
-        <br />
-        <div style=" margin-left:auto;">
-            <!-- Search Widget -->
-          <form name="search" action="search.php" method="post">
-              <div class="input-group">
-                  <input type="text" name="searchtitle" class="form-control" placeholder="Search for..." required>
-                    <span class="input-group-btn">
-                      <button class="btn btn-secondary" type="submit">Go!</button>
-                    </span>
-                </div>
-            </form>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">BERITA</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link text-white dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              DEPARTEMEN
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+              <li><a class="dropdown-item" href="#">MIKAT</a></li>
+              <li><a class="dropdown-item" href="#">KESEKRETARIATAN TEKNIK ELEKTRO</a></li>
+              <li><a class="dropdown-item" href="#">PENDIDIKAN DAN PENGEMBANGAN SUMBER DAYA MAHASISWA</a></li>
+              <li><a class="dropdown-item" href="#">KEROHANIAN</a></li>
+              <li><a class="dropdown-item" href="#">KESMA</a></li>
+              <li><a class="dropdown-item" href="#">Bidang F</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">DOKUMEN</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">PROFIL</a>
+          </li>
+        </ul>
+        <form class="d-flex" action="search.php" method="post">
+          <input class="form-control me-2" type="search" name="searchtitle" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-primary btn-color-blue" type="submit">Search</button>
+        </form>
       </div>
     </div>
   </nav>
